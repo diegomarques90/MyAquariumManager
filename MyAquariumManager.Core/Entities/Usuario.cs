406393202707af7b7246cdf0f73e5d3f4ca9795b
Diego Marques <rzrsdiego@gmail.com>
2025-05-10 15:52:42 -0300

    feat[MAM-0001]: Instalação do Identity e uso na classe Usuario

--- MyAquariumManager.Core/Entities/Usuario.cs
+++ MyAquariumManager.Core/Entities/Usuario.cs
@@ -1,17 +1,12 @@
-﻿using MyAquariumManager.Core.Enums;
+﻿using Microsoft.AspNetCore.Identity;
+using MyAquariumManager.Core.Enums;
 
 namespace MyAquariumManager.Core.Entities
 {
-    public class Usuario(string usuarioCriacao) : BaseEntity(usuarioCriacao)
+    public class Usuario : IdentityUser
     {
         public string Nome { get; private set; }
-        public string Email { get; private set; }
         public string Documento { get; private set; }
         public TipoUsuario TipoUsuario { get; private set; }
-
-        public override void Validar()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
