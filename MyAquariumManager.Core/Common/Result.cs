f64b7e40d940fb5659ac6b77379fb34af6757591
Diego Marques <rzrsdiego@gmail.com>
2025-05-22 14:41:29 -0300

    Implementacao inicial do Result Pattern

---
+++ MyAquariumManager.Core/Common/Result.cs
@@ -0,0 +1,25 @@
+﻿using MyAquariumManager.Core.Enums;
+
+namespace MyAquariumManager.Core.Common
+{
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public HttpCode HttpCode { get; }
+        public List<string> Errors { get; }
+
+        protected Result(bool isSuccess, HttpCode httpCode, List<string> errors)
+        {
+            IsSuccess = isSuccess;
+            HttpCode = httpCode;
+            Errors = errors;
+        }
+
+        public static Result Success(HttpCode httpCode) => new Result(true, httpCode, []);
+
+        public static Result Failure(HttpCode httpCode, List<string> errors) => new Result(false, httpCode, errors);
+
+        public static Result Failure(HttpCode httpCode, string error) => new Result(false, httpCode, [error]);
+    }
+}
---
+++ MyAquariumManager.Core/Common/ResultValue.cs
@@ -0,0 +1,31 @@
+﻿using MyAquariumManager.Core.Enums;
+
+namespace MyAquariumManager.Core.Common
+{
+    public class ResultValue<TValue> : Result
+    {
+        private readonly TValue _value;
+
+        public TValue Value 
+        { 
+            get
+            {
+                if (IsFailure)
+                    throw new InvalidOperationException("Não é possível acessar o valor de um resultado de falha.");
+                
+                return _value;
+            }
+        }
+
+        protected ResultValue(TValue value, bool isSuccess, HttpCode httpCode, List<string> errors) : base(isSuccess, httpCode, errors)
+        {
+            _value = value;
+        }
+
+        public static ResultValue<TValue> Success(TValue value, HttpCode httpCode) => new ResultValue<TValue>(value, true, httpCode, []);
+
+        public new static ResultValue<TValue> Failure(HttpCode httpCode, List<string> errors) => new ResultValue<TValue>(default, false, httpCode, errors);
+
+        public new static ResultValue<TValue> Failure(HttpCode httpCode, string error) => new ResultValue<TValue>(default, false, httpCode, [error]);
+    }
+}
