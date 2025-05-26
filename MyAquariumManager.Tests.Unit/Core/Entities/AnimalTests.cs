dbff68fcc320270bfb79496c9b279a2c5ba78302
Diego Marques <rzrsdiego@gmail.com>
2025-05-26 16:54:34 -0300

    feat[MAM-0001]: Criação do projeto de testes de unidade

---
+++ MyAquariumManager.Tests.Unit/Core/Entities/AnimalTests.cs
@@ -0,0 +1,18 @@
+﻿namespace MyAquariumManager.Tests.Unit.Core.Entities
+{
+    public class AnimalTests
+    {
+        [Fact]
+        public void CriarAnimal_DeveRetornarSucesso_QuandoAnimalEhValido()
+        {
+            // Arrange
+            
+
+            // Act
+
+
+            // Assert
+
+        }
+    }
+}
