98eda6c6263bdff438c216f73b461073a93b7a8d
Diego Marques <rzrsdiego@gmail.com>
2025-06-25 16:24:34 -0300

    feat[MAM-0002]: Implementa o Authorize e configuracao padrao para redirecionar para o login

--- MyAquariumManager.Web/Program.cs
+++ MyAquariumManager.Web/Program.cs
@@ -7,6 +7,7 @@ using MyAquariumManager.Infrastructure.Data.Repositories;
 using MyAquariumManager.Web.Filters;
 using MyAquariumManager.Web.Validators.Animal;
 using MyAquariumManager.Core.Entities;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,11 +17,20 @@ builder.Services.AddControllersWithViews();
 var connectionString = builder.Configuration.GetConnectionString("MAMConnection");
 builder.Services.AddDbContext<MyAquariumManagerDbContext>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddDefaultIdentity<Usuario>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<MyAquariumManagerDbContext>();
+builder.Services.AddDefaultIdentity<Usuario>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<MyAquariumManagerDbContext>()
+    .AddDefaultUI()
+    .AddDefaultTokenProviders(); 
 
 builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
 builder.Services.AddScoped<IAnimalRepository, AnimalRepository>();
-
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Identity/Account/Login"; 
+    //options.AccessDeniedPath = "/Identity/Account/AccessDenied"; // Caminho para acesso negado
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(15);
+    options.SlidingExpiration = true;
+});
 
 builder.Services.AddAutoMapper(typeof(AnimalProfile).Assembly);
 
@@ -45,7 +55,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
