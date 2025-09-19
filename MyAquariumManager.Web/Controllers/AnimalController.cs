b5b1b5d7795ec65a4062e3b80f82ed8fea926bf5
DiegoAraujo <diego.araujo@syngenta.com>
2025-09-19 16:35:53 -0300

    feat[MAM-0002]: Implementa validacoes no formulario, lib toastr e preparo inicial para a requisicao ajax

--- MyAquariumManager.Web/Controllers/AnimalController.cs
+++ MyAquariumManager.Web/Controllers/AnimalController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using MyAquariumManager.Application.DTOs.Animal;
 using MyAquariumManager.Application.Interfaces.Services;
 
 namespace MyAquariumManager.Web.Controllers
 {
-    [Authorize] 
+    [Authorize]
     public class AnimalController(IAnimalService animalService) : Controller
     {
         private readonly IAnimalService _animalService = animalService;
@@ -24,6 +25,22 @@ namespace MyAquariumManager.Web.Controllers
                 return Json(new { data = new List<AnimalDto>() });
 
             return Json(new { data = result.Value });
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost("cadastrar-animal")]
+        public async Task<IActionResult> CadastrarAnimal([FromBody] CriarAnimalDto model)
+        {
+            var result = await _animalService.CadastrarAnimalAsync(model);
+
+            if (result.IsFailure)
+                return BadRequest(new { success = false, errors = result.Errors });
+
+            return Ok(new { success = true, result.Value.Id });
         }
     }
 }
