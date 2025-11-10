1832b512f2b4163268152b255f0732382506742f
DiegoAraujo <diego.araujo@syngenta.com>
2025-11-10 16:44:38 -0300

    feat[MAM-0002]: Implementa testes e início do front da entidade planta

---
+++ MyAquariumManager.Tests.Unit/Web/Validators/AtualizarPlantaDtoValidatorTests.cs
@@ -0,0 +1,151 @@
+﻿using MyAquariumManager.Web.Validators.Planta;
+
+namespace MyAquariumManager.Tests.Unit.Web.Validators
+{
+    public class AtualizarPlantaDtoValidatorTests
+    {
+        private readonly AtualizarPlantaDtoValidator _validator = new AtualizarPlantaDtoValidator();
+
+        [Fact]
+        public void SemFalhas_Quando_AtualizarPlantaDtoForValido()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void DeveTerFalha_Quando_IdInvalido()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void DeveTerFalha_Quando_NomeVazio()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void DeveTerFalha_Quando_Nome_ExcederCaracteres()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void DeveTerFalha_Quando_NomeCientifico_ExcederCaracteres()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void DeveTerFalha_Quando_LocalAquisicao_ExcederCaracteres()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void DeveTerFalha_Quando_DataAquisicao_ForFutura()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void DeveTerFalha_Quando_FaixaDeTamanho_ExcederCaracteres()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [Fact]
+        public void DeveTerFalha_Quando_TipoDeCrescimento_ForInvalido()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [Fact]
+        public void DeveTerFalha_Quando_TipoDeIluminacao_ForInvalido()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [Fact]
+        public void DeveTerFalha_Quando_FaixaDoPH_ExcederCaracteres()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [Fact]
+        public void DeveTerFalha_Quando_FaixaDeTemperatura_ExcederCaracteres()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [Fact]
+        public void DeveTerFalha_Quando_NivelDeCultivo_ExcederCaracteres()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [Fact]
+        public void DeveTerFalha_Quando_TipoDePlantio_ForInvalido()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [Fact]
+        public void DeveTerFalha_Quando_FormaDeReproducao_ExcederCaracteres()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [Fact]
+        public void DeveTerFalha_Quando_InformacoesAdicionais_ExcederCaracteres()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
---
+++ MyAquariumManager.Web/Validators/Planta/AtualizarPlantaDtoValidator.cs
@@ -0,0 +1,61 @@
+﻿using FluentValidation;
+using MyAquariumManager.Application.DTOs.Planta;
+using MyAquariumManager.Core.Constants;
+
+namespace MyAquariumManager.Web.Validators.Planta
+{
+    public class AtualizarPlantaDtoValidator : AbstractValidator<AtualizarPlantaDto>
+    {
+        public AtualizarPlantaDtoValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage(BaseConstants.ID_NAO_PODE_SER_NULO_OU_VAZIO);
+
+            RuleFor(x => x.Nome)
+                .NotEmpty().WithMessage(BaseConstants.NOME_OBRIGATORIO)
+                .MaximumLength(200).WithMessage(BaseConstants.NOME_QUANTIDADE_MAXIMA);
+
+            RuleFor(x => x.NomeCientifico)
+                .MaximumLength(200).WithMessage(BaseConstants.NOME_CIENTIFICO_QUANTIDADE_MAXIMA);
+
+            RuleFor(x => x.LocalAquisicao)
+                .MaximumLength(100).WithMessage(BaseConstants.LOCAL_AQUISICAO_QUANTIDADE_MAXIMA);
+
+            When(x => x.DataAquisicao.HasValue, () => 
+            {
+                RuleFor(x => x.DataAquisicao)
+                    .LessThan(DateTime.Now).WithMessage(BaseConstants.DATA_AQUISICAO_NAO_PODE_SER_FUTURA);
+            });
+
+            RuleFor(x => x.FaixaDeTamanho)
+                .MaximumLength(100).WithMessage(BaseConstants.FAIXA_TAMANHO_QUANTIDADE_MAXIMA);
+
+            RuleFor(x => x.TipoDeCrescimento)
+                .NotNull().WithMessage(BaseConstants.TIPO_CRESCIMENTO_OBRIGATORIO)
+                .IsInEnum().WithMessage(BaseConstants.TIPO_CRESCIMENTO_INVALIDO);
+
+            RuleFor(x => x.TipoDeIluminacao)
+                .NotNull().WithMessage(BaseConstants.TIPO_ILUMINACAO_OBRIGATORIO)
+                .IsInEnum().WithMessage(BaseConstants.TIPO_ILUMINACAO_INVALIDO);
+
+            RuleFor(x => x.FaixaDoPH)
+                .MaximumLength(100).WithMessage(BaseConstants.FAIXA_PH_QUANTIDADE_MAXIMA);
+
+            RuleFor(x => x.FaixaDeTemperatura)
+                .MaximumLength(100).WithMessage(BaseConstants.FAIXA_TEMPERATURA_QUANTIDADE_MAXIMA);
+
+            RuleFor(x => x.NivelDeCutivo)
+                .MaximumLength(100).WithMessage(BaseConstants.NIVEL_CULTIVO_QUANTIDADE_MAXIMA);
+
+            RuleFor(x => x.TipoDePlantio)
+                .NotNull().WithMessage(BaseConstants.TIPO_PLANTIO_OBRIGATORIO)
+                .IsInEnum().WithMessage(BaseConstants.TIPO_PLANTIO_INVALIDO);
+
+            RuleFor(x => x.FormaDeReproducao)
+                .MaximumLength(100).WithMessage(BaseConstants.FORMA_REPRODUCAO_QUANTIDADE_MAXIMA);
+
+            RuleFor(x => x.InformacoesAdicionais)
+              .MaximumLength(800).WithMessage(BaseConstants.INFORMACOES_ADICIONAIS_QUANTIDADE_MAXIMA);
+        }
+    }
+}
